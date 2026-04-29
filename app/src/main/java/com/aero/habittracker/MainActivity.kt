c846ef750be346cfed1933ae96b374e281140786
JamesCordova <jcordovas@gmail.com>
2026-04-29 15:46:49 -0500

    Pass application context to HabitTrackerScreen in MainActivity

--- app/src/main/java/com/aero/habittracker/MainActivity.kt
+++ app/src/main/java/com/aero/habittracker/MainActivity.kt
@@ -3,14 +3,7 @@ package com.aero.habittracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import com.aero.habittracker.ui.habits.HabitTrackerScreen
 import com.aero.habittracker.ui.theme.HabitTrackerTheme
 
@@ -19,7 +12,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HabitTrackerTheme {
-                HabitTrackerScreen()
+                val application = LocalContext.current.applicationContext as HabitTrackerApplication
+                HabitTrackerScreen(application)
             }
         }
     }
