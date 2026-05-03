b82c8a725afe597828fbdccaba8e7e7af34aad6e
JamesCordova <jcordovas@gmail.com>
2026-05-03 13:22:58 -0500

    Add safeDrawingPadding to WelcomeScreen for improved layout handling

--- app/src/main/java/com/aero/habittracker/ui/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/aero/habittracker/ui/welcome/WelcomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -26,6 +27,7 @@ fun WelcomeScreen(
         modifier = modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
+            .safeDrawingPadding()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
