98f901076d1d5a839bada16972eb14bbec3549a9
JamesCordova <jcordovas@gmail.com>
2026-05-03 15:34:46 -0500

    Add rounded corners to TextField in InputNewHabit

--- app/src/main/java/com/aero/habittracker/ui/habits/components/InputNewHabit.kt
+++ app/src/main/java/com/aero/habittracker/ui/habits/components/InputNewHabit.kt
@@ -3,6 +3,7 @@ package com.aero.habittracker.ui.habits.components
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,6 +15,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -28,6 +30,7 @@ fun InputNewHabit(
             value = input,
             onValueChange = onInputChange,
             modifier = Modifier.weight(1f),
+            shape = RoundedCornerShape(50),
             placeholder = { Text("Nuevo hábito") }
         )
 
