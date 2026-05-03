4531e171b4b949aa228976d6009bf7ad62855c26
JamesCordova <jcordovas@gmail.com>
2026-05-03 12:21:07 -0500

    Replace HabitTrackerScreen with AppNavigation in MainActivity for improved navigation structure

--- app/src/main/java/com/aero/habittracker/MainActivity.kt
+++ app/src/main/java/com/aero/habittracker/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.platform.LocalContext
-import com.aero.habittracker.ui.habits.HabitTrackerScreen
+import com.aero.habittracker.ui.navigation.AppNavigation
 import com.aero.habittracker.ui.theme.HabitTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HabitTrackerTheme {
                 val application = LocalContext.current.applicationContext as HabitTrackerApplication
-                HabitTrackerScreen(application)
+                AppNavigation(application)
             }
         }
     }
