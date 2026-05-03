cd9a6a63b8b3108430f6471823559b2d93c779f1
JamesCordova <jcordovas@gmail.com>
2026-05-03 15:05:13 -0500

    Replace delete Button with IconButton in HabitItem

--- app/src/main/java/com/aero/habittracker/ui/habits/components/HabitItem.kt
+++ app/src/main/java/com/aero/habittracker/ui/habits/components/HabitItem.kt
@@ -8,12 +8,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.aero.habittracker.domain.Habit
 
@@ -50,8 +57,14 @@ fun HabitItem(
                 style = MaterialTheme.typography.bodyLarge
             )
         }
-        Button(onClick = onDelete) {
-            Text("Eliminar")
+        IconButton(
+            onClick = onDelete
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Delete,
+                contentDescription = "Agregar hábito",
+                tint = MaterialTheme.colorScheme.error
+            )
         }
     }
 }
